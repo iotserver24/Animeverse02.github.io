36becd1f504dbb1ed12433c5a2555a22bc3ceed1
R3AP3Redit <147928812+iotserver24@users.noreply.github.com>
2024-05-16 22:18:10 +0530

    Update anime.js

--- js/anime.js
+++ js/anime.js
@@ -7,11 +7,7 @@ const recommendationsapi = "/recommendations/";
 // Api Server Manager
 const AvailableServers = ['https://api3.iotserver24.workers.dev/'];
 
-//  const AvailableServers = [
- //   "https://api1.anime-dex.workers.dev",
-//    "https://api2.anime-dex.workers.dev",
-//    "https://api3.anime-dex.workers.dev",
-// ];
+
 
 function getApiServer() {
     return AvailableServers[Math.floor(Math.random() * AvailableServers.length)];
