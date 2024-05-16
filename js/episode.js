08f8811ec252d0a5a43f4453d8cbb4318a10c013
R3AP3Redit <147928812+iotserver24@users.noreply.github.com>
2024-05-16 22:27:13 +0530

    Update episode.js

--- js/episode.js
+++ js/episode.js
@@ -8,11 +8,6 @@ const dlapi = "/download/";
 // Api Server Manager
 const AvailableServers = ["https://api3.iotserver24.workers.dev"];
 
-// const AvailableServers = [
-//   "https://api1.anime-dex.workers.dev",
-//    "https://api2.anime-dex.workers.dev",
-//    "https://api3.anime-dex.workers.dev",
-// ];
 
 function getApiServer() {
     return AvailableServers[Math.floor(Math.random() * AvailableServers.length)];
